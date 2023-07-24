77e7ebdfec822e1ac8339ff69cf7bc3eadf3f5a4
Natalie Benjes <natalie.benjes@gmail.com>
2023-07-24 16:39:13 -0700

    update to EFCORE

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -1,35 +1,39 @@
-// using Microsoft.AspNetCore.Mvc;
-// using ToDoList.Models;
-// using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using ToDoList.Models;
+using System.Collections.Generic;
+using System.Linq;
 
-// namespace ToDoList.Controllers
-// {
-//   public class ItemsController : Controller
-//   {
+namespace ToDoList.Controllers
+{
+  public class ItemsController : Controller
+  {
+    private readonly ToDoListContext _db;
 
-//     [HttpGet("/categories/{categoryId}/items/new")]
-//     public ActionResult New(int categoryId)
-//     {
-//       Category category = Category.Find(categoryId);
-//       return View(category);
-//     }
-
-//     [HttpPost("/items/delete")]
-//     public ActionResult DeleteAll()
-//     {
-//       Item.ClearAll();
-//       return View();
-//     }
-
-//     [HttpGet("/categories/{categoryId}/items/{itemId}")]
-//     public ActionResult Show(int categoryId, int itemId)
-//     {
-//       Item item = Item.Find(itemId);
-//       Category category = Category.Find(categoryId);
-//       Dictionary<string, object> model = new Dictionary<string, object>();
-//       model.Add("item", item);
-//       model.Add("category", category);
-//       return View(model);
-//     }
-//   }
-// }
+    public ItemsController(ToDoListContext db)
+    {
+      _db = db;
+    }
+    public ActionResult Create()
+    {
+      return View();
+    }
+    [HttpPost]
+    //takes item as an argument and add it to the Items DbSet
+    public ActionResult Create(Item item)
+    {
+      _db.Items.Add(item);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+      public ActionResult Index()
+    {
+      List<Item> model = _db.Items.ToList();
+      return View(model);
+    }
+    public ActionResult Details(int id)
+    {
+      Item thisItem = _db.Items.FirstOrDefault(item => item.ItemId == id);
+      return View(thisItem);
+    }
+  }
+}
